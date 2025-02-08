45c1c76b590a85ec2f9a1a50495c625814b9599d
Ishwar Aru <eshwararu5@gmail.com>
2025-02-08 11:54:33 -0500

    Added custom user detailes file

---
+++ src/main/java/com/example/demo/services/MyUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.example.demo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.User;
+import com.example.demo.model.UserPrinciple;
+import com.example.demo.repositories.UserRepository;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User userData = userRepository.findByEmailAddress(username)
+            .orElseThrow(() -> new UsernameNotFoundException("Your are not registered"));
+
+        return new UserPrinciple(userData);
+    }
+
+}
