6d6b249ea7e967253da463899bdf00df7dbe9f02
Ishwar Aru <eshwararu5@gmail.com>
2025-02-08 12:02:57 -0500

    New user principles for login

---
+++ src/main/java/com/example/demo/model/UserPrinciple.java
@@ -0,0 +1,32 @@
+package com.example.demo.model;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class UserPrinciple implements UserDetails {
+
+    private User user;
+
+    public UserPrinciple(User userData) {
+        this.user = userData;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getUserPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUserEmail();
+    }
+
+}
