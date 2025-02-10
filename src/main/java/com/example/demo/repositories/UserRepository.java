791fdcfd238326fb03a8909e01cd3a38760441ae
Ishwar Aru <eshwararu5@gmail.com>
2025-02-10 17:47:37 -0500

    some new changes

--- src/main/java/com/example/demo/repositories/UserRepository.java
+++ src/main/java/com/example/demo/repositories/UserRepository.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.model.User;
@@ -12,6 +13,6 @@ import com.example.demo.model.User;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     @Query(value = "SELECT * FROM user_data_table WHERE user_email_address = :emailAddress", nativeQuery = true)
-    Optional<User> findByEmailAddress(String emailAddress);
+    Optional<User> findByEmailAddress(@Param("emailAddress") String emailAddress);
 
 }
