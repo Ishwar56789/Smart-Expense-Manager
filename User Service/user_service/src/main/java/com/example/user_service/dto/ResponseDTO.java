3a15bab576e9d70d5915b38b2140fc9adcd56aea
Ishwar Aru <eshwararu5@gmail.com>
2025-08-27 10:41:19 -0400

    Crate a custom response dto for better clarification and messaging

---
+++ User Service/user_service/src/main/java/com/example/user_service/dto/ResponseDTO.java
@@ -0,0 +1,22 @@
+package com.example.user_service.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseDTO {
+
+    private LocalDateTime timeStamp;
+    
+    private String message;
+    
+    private int status;
+    
+    private Object data;
+
+}
