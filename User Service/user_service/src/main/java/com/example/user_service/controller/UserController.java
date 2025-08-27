8e8df5be3174a48dea500af303e66e3258f0f95e
Ishwar Aru <eshwararu5@gmail.com>
2025-08-27 10:33:49 -0400

    Add new sign up api for user registration

--- User Service/user_service/src/main/java/com/example/user_service/controller/UserController.java
+++ User Service/user_service/src/main/java/com/example/user_service/controller/UserController.java
@@ -1,5 +1,30 @@
 package com.example.user_service.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.user_service.dto.SignUpDto;
+import com.example.user_service.handler.ResponseHandler;
+import com.example.user_service.service.UserService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/v1/user")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/sign-up")
+    public ResponseEntity<?> userRegistration(@Valid @RequestBody SignUpDto userData) {
+        userService.saveUserData(userData);
+        return ResponseHandler.responseBuilder(null, "User registered succesfully", HttpStatus.OK);
+    }
 
 }
