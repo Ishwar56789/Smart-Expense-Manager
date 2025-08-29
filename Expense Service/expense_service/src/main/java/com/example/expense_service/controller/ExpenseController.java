00d02ab853cf4a2dbaa699687384f06c901b2468
Ishwar Aru <eshwararu5@gmail.com>
2025-08-29 19:13:05 -0400

    Create a new ExpenseController class to handle expense api's

---
+++ Expense Service/expense_service/src/main/java/com/example/expense_service/controller/ExpenseController.java
@@ -0,0 +1,10 @@
+package com.example.expense_service.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/expense")
+public class ExpenseController {
+
+}
