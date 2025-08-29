0c97dd1db4bcf98fc75b851806e1a56d344cc40e
Ishwar Aru <eshwararu5@gmail.com>
2025-08-29 16:17:40 -0400

    Create user principle class to get user credentials and authority

---
+++ User Service/user_service/src/main/java/com/example/user_service/principle/UserPrinciple.java
@@ -0,0 +1,34 @@
+package com.example.user_service.principle;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.example.user_service.model.User;
+
+public class UserPrinciple implements UserDetails {
+
+    private User user;
+
+    public UserPrinciple(User userData) {
+        this.user = userData;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getUserPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUserEmail();
+    }
+
+}
