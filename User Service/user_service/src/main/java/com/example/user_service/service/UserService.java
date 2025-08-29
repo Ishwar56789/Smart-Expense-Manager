c320e8cbbe2a4bff316c5f87f9651ab0aa3e837f
Ishwar Aru <eshwararu5@gmail.com>
2025-08-29 15:53:20 -0400

    Using bCryptPasswordEncoder method for password hashing

--- User Service/user_service/src/main/java/com/example/user_service/service/UserService.java
+++ User Service/user_service/src/main/java/com/example/user_service/service/UserService.java
@@ -3,6 +3,7 @@ package com.example.user_service.service;
 import java.time.LocalDate;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 // import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
@@ -16,12 +17,15 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    
 
     private User dtoToEntity(SignUpDTO userData) {
         User user = new User();
         user.setUserName(userData.getUserName());
         user.setUserEmail(userData.getUserEmail());
-        user.setUserPassword(userData.getUserPassword());
+        user.setUserPassword(bCryptPasswordEncoder.encode(userData.getUserPassword()));
         user.setUserCurrencyPrefrence(userData.getUserCurrencyPrefrence());
         user.setUserAccountCreatedAt(LocalDate.now());
         // user.setUserAccountLastUpdatedAt(null);
