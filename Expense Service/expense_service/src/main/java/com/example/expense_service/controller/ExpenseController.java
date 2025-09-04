3e4878b84d9e6645e62d67ce8f13444b84e8400e
Ishwar Aru <eshwararu5@gmail.com>
2025-09-04 09:58:24 -0400

    Create a new api (/add-expense) to save expense data

--- Expense Service/expense_service/src/main/java/com/example/expense_service/controller/ExpenseController.java
+++ Expense Service/expense_service/src/main/java/com/example/expense_service/controller/ExpenseController.java
@@ -1,10 +1,30 @@
 package com.example.expense_service.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.expense_service.dtos.AddExpenseDTO;
+import com.example.expense_service.handler.ResponseHandler;
+import com.example.expense_service.services.ExpenseService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/expense")
 public class ExpenseController {
+
+    @Autowired 
+    private ExpenseService expenseService;
+
+    @PostMapping("/add-expense")
+    public ResponseEntity<?> addExpense(@RequestBody @Valid AddExpenseDTO expenseData) {
+        expenseService.saveExpense(expenseData);
+        return ResponseHandler.resposeBuilder(null, "Expense added successfully", HttpStatus.CREATED);
+    }
 
 }
