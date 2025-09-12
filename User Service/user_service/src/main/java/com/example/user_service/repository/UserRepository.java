64f0a896010708ea7eeb895c9befc3ad2b204cd8
Ishwar Aru <eshwararu5@gmail.com>
2025-09-12 10:53:40 -0400

    feat: add new existsByUserEmail method to check if the email is present

--- User Service/user_service/src/main/java/com/example/user_service/repository/UserRepository.java
+++ User Service/user_service/src/main/java/com/example/user_service/repository/UserRepository.java
@@ -3,8 +3,6 @@ package com.example.user_service.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.user_service.model.User;
@@ -12,10 +10,8 @@ import com.example.user_service.model.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    @Query(
-        value = "SELECT * FROM user_data_table WHERE user_email_address = :userEmail",
-        nativeQuery = true
-    )
-    Optional<User> findByEmailAddress(@Param("userEmail") String userEmail);
+    Optional<User> findByUserEmail(String userEmail);
+
+    boolean existsByUserEmail(String userEmail);
 
 }
