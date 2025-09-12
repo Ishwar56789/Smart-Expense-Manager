914aa843257d9ab0a8ff452649e1f623a2c01c25
Ishwar Aru <eshwararu5@gmail.com>
2025-09-12 15:46:10 -0400

    feat: new '/all-expenses' api endpoint to get all expenses

--- Expense Service/expense_service/src/main/java/com/example/expense_service/controller/ExpenseController.java
+++ Expense Service/expense_service/src/main/java/com/example/expense_service/controller/ExpenseController.java
@@ -1,8 +1,11 @@
 package com.example.expense_service.controller;
 
+import java.time.LocalDate;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +28,14 @@ public class ExpenseController {
     public ResponseEntity<?> addExpense(@RequestBody @Valid AddExpenseDTO expenseData) {
         expenseService.saveExpense(expenseData);
         return ResponseHandler.resposeBuilder(null, "Expense added successfully", HttpStatus.CREATED);
+    }
+
+
+    @GetMapping("/all-expenses")
+    public ResponseEntity<?> getAllExpenses() {
+        return ResponseHandler.resposeBuilder(
+            expenseService.getAllExpenses(), "All expenses fetched successfully till " + LocalDate.now(), HttpStatus.OK
+        );
     }
 
 }
