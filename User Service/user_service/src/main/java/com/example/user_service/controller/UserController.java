1725c737fd72a9efbdedb20937384abea5a0cb9c
Ishwar Aru <eshwararu5@gmail.com>
2025-09-12 10:47:32 -0400

    Throws UserAlreadyExistsException for userRegistration method

--- User Service/user_service/src/main/java/com/example/user_service/controller/UserController.java
+++ User Service/user_service/src/main/java/com/example/user_service/controller/UserController.java
@@ -3,12 +3,14 @@ package com.example.user_service.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.user_service.dto.SignUpDto;
+import com.example.user_service.dto.SignUpDTO;
+import com.example.user_service.exceptions.UserAlreadyExistsException;
 import com.example.user_service.handler.ResponseHandler;
 import com.example.user_service.service.UserService;
 
@@ -21,8 +23,13 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @GetMapping("/message")
+    public ResponseEntity<?> message() {
+        return ResponseHandler.responseBuilder(null, "This is a message testing API", HttpStatus.OK);
+    }
+
     @PostMapping("/sign-up")
-    public ResponseEntity<?> userRegistration(@Valid @RequestBody SignUpDto userData) {
+    public ResponseEntity<?> userRegistration(@Valid @RequestBody SignUpDTO userData) throws UserAlreadyExistsException {
         userService.saveUserData(userData);
         return ResponseHandler.responseBuilder(null, "User registered succesfully", HttpStatus.OK);
     }
