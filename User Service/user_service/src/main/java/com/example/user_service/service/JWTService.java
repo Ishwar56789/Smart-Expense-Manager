4099b0800d7c983e25347163fc7a610266566826
Ishwar Aru <eshwararu5@gmail.com>
2025-09-22 16:18:57 -0400

    feat(security): add methods to extract claims from jwt token

--- User Service/user_service/src/main/java/com/example/user_service/service/JWTService.java
+++ User Service/user_service/src/main/java/com/example/user_service/service/JWTService.java
@@ -3,12 +3,15 @@ package com.example.user_service.service;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 import javax.crypto.SecretKey;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
@@ -43,6 +46,38 @@ public class JWTService {
     private SecretKey getKey() {
         byte[] keyBytes = Decoders.BASE64.decode(jwtSecretKey);
         return Keys.hmacShaKeyFor(keyBytes);
-    } 
+    }
+
+
+    public String extractUserEmail(String token) {
+        return extractClaims(token, Claims::getSubject);
+    }
+
+
+    private <T> T extractClaims(String token, Function<Claims, T> claimResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimResolver.apply(claims); 
+    }
+
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser().verifyWith(getKey()).build().parseSignedClaims(token).getPayload();
+    }
+
+
+    public boolean validateToken(String token, UserDetails userDetails) {
+        final String email = extractUserEmail(token);
+        return email.equals(userDetails.getUsername()) && !isTokenExpired(token);
+    }
+
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+
+    private Date extractExpiration(String token) {
+        return extractClaims(token, Claims::getExpiration);
+    }
 
 }
