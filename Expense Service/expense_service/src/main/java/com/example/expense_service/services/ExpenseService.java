60b319f6080376ac75b72577a3f63af5b9020e65
Ishwar Aru <eshwararu5@gmail.com>
2025-09-23 16:35:35 -0400

    feat(expense service): extracting userId from header to get the expense data of the user

--- Expense Service/expense_service/src/main/java/com/example/expense_service/services/ExpenseService.java
+++ Expense Service/expense_service/src/main/java/com/example/expense_service/services/ExpenseService.java
@@ -1,12 +1,14 @@
 package com.example.expense_service.services;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.expense_service.dtos.AddExpenseDTO;
 import com.example.expense_service.dtos.ExpenseDTO;
+import com.example.expense_service.filter.JWTFilter;
 import com.example.expense_service.model.Expense;
 import com.example.expense_service.repository.ExpenseRepository;
 
@@ -16,8 +18,11 @@ public class ExpenseService {
     @Autowired
     private ExpenseRepository expenseRepository;
 
+    @Autowired
+    private JWTFilter jwtFilter;
 
-    private Expense dtoToExpense(AddExpenseDTO expenseData) {
+
+    private Expense dtoToExpense(AddExpenseDTO expenseData, Long userID) {
         Expense expense = new Expense();
         expense.setExpenseTitle(expenseData.getExpenseTitle());
         expense.setExpenseAmount(expenseData.getExpenseAmount());
@@ -26,31 +31,37 @@ public class ExpenseService {
         expense.setExpenseCategory(expenseData.getExpenseCategory());
         expense.setExpensePaymentMethod(expenseData.getExpensePaymentMethod());
         expense.setExpenseRegistrationDate(LocalDate.now());
-        expense.setUserId(1L); // Placeholder for user ID, to be replaced with actual user context
+        expense.setUserId(userID); // Placeholder for user ID, to be replaced with actual user context
         return expense;
     }
 
-    public void saveExpense(AddExpenseDTO expenseData) {
-        Expense expense = dtoToExpense(expenseData);
+    public void saveExpense(AddExpenseDTO expenseData, String authHeader) {
+        Long userID = jwtFilter.extractUserID(authHeader);
+
+        Expense expense = dtoToExpense(expenseData, userID);
         expenseRepository.save(expense);
     }
 
 
-    private ExpenseDTO expenseToDTO(Expense expense) {
-        ExpenseDTO expenseDTO = new ExpenseDTO();
-        expenseDTO.setExpenseTitle(expense.getExpenseTitle());
-        expenseDTO.setExpenseAmount(expense.getExpenseAmount());
-        expenseDTO.setExpenseDescription(expense.getExpenseDescription());
-        expenseDTO.setExpenseSpendingDate(expense.getExpenseSpendingDate());
-        expenseDTO.setExpenseCategory(expense.getExpenseCategory());
-        expenseDTO.setExpensePaymentMethod(expense.getExpensePaymentMethod());
-        expenseDTO.setExpenseRegistrationDate(expense.getExpenseRegistrationDate());
-        expenseDTO.setExpenseUpdateDate(expense.getExpenseUpdateDate());
-        return expenseDTO;
+    private ExpenseDTO expenseToDTO(Expense expenseData) {
+        return new ExpenseDTO(
+            expenseData.getExpenseTitle(),
+            expenseData.getExpenseAmount(),
+            expenseData.getExpenseDescription(),
+            expenseData.getExpenseSpendingDate(),
+            expenseData.getExpenseCategory(),
+            expenseData.getExpensePaymentMethod(),
+            expenseData.getExpenseRegistrationDate(),
+            expenseData.getExpenseUpdateDate()
+        );
     }
 
-    public Object getAllExpenses() {
-        return expenseRepository.findAll().stream().map(this::expenseToDTO).toList();
+    public Object getAllExpenses(String authHeader) {
+        Long userID = jwtFilter.extractUserID(authHeader);
+
+        List<Expense> expenseData = expenseRepository.findByUserId(userID);
+
+        return expenseData.stream().map(this::expenseToDTO).toList();
     }
 
 }
