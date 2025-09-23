1f2d868312b4ea706bb52a2b3c15d6a640505e8f
Ishwar Aru <eshwararu5@gmail.com>
2025-09-23 14:38:21 -0400

    feat(expense-service): accept Authorization header to call user service api

--- Expense Service/expense_service/src/main/java/com/example/expense_service/feign_client/SEMUserServiceFeignClient.java
+++ Expense Service/expense_service/src/main/java/com/example/expense_service/feign_client/SEMUserServiceFeignClient.java
@@ -3,11 +3,12 @@ package com.example.expense_service.feign_client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient(name = "SEM-USER-SERVICE", path = "/api/v1/user")
 public interface SEMUserServiceFeignClient {
 
     @GetMapping("/message")
-    public ResponseEntity<?> messageAPIFromUserService();
+    public ResponseEntity<?> messageAPIFromUserService(@RequestHeader("Authorization") String authHeader);
 
 }
