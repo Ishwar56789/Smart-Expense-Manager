76e5d571091d139b373813f7ef64112d2242b9c2
Ishwar Aru <eshwararu5@gmail.com>
2025-09-23 16:33:14 -0400

    feat(expense service): add findByUserId method to get the expense data of the user

--- Expense Service/expense_service/src/main/java/com/example/expense_service/repository/ExpenseRepository.java
+++ Expense Service/expense_service/src/main/java/com/example/expense_service/repository/ExpenseRepository.java
@@ -1,5 +1,7 @@
 package com.example.expense_service.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.example.expense_service.model.Expense;
 
 @Repository
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+
+    List<Expense> findByUserId(Long userID);
 
 }
